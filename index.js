349e387fbeef022c3e165af7d7ae8dfee7e6a6f2
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2025-02-28 19:32:02 +0600

    added index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+const {MONGO_DB_CONFIG} = require ("./config/app.config")
+const http = require("http")
+const server = http.createServer(app)
+
+
+//meeting-server
+//initMeetingServer(server)
+
+mongoose.Promise = global.Promise;
+mongoose.connect(MONGO_DB_CONFIG, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(()=>{
+    console.log("Database Connected")
+}),(error)=>{
+    console.log("Database can't be Connected")
+}
+
+app.use(express.json());
+app.use("/api", require("./routes/app.route"))
+server.listen(process.env.port || 4000, function(){
+    console.log("Reday to go!")
+})
